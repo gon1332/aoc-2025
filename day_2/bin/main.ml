ad555ba924d9a5b6df261827099583c7de7c3c2b
Ioannis Konstantelias <ikonstadel@gmail.com>
2025-12-07 20:02:22 +0000

    Day 2 is officially completed!

--- day_2/bin/main.ml
+++ day_2/bin/main.ml
@@ -1,8 +1,8 @@
 open Day2
 
-let read_channel ic =
+let read_channel ic strategy =
         try
-                let act range = Day2.range_from_str range |> Day2.sum_of_invalid_ids in
+                let act range = Day2.range_from_str range |> strategy in
                 input_line ic |> String.split_on_char ',' |> List.map act |> List.fold_left ( + ) 0
         with e ->
                 match e with
@@ -17,6 +17,8 @@ let () =
                 let ic = open_in file in
                 let finally () = close_in ic in
                 let work () =
-                        Printf.printf "Sum of invalid IDs %i\n" @@ read_channel ic;
+                        Printf.printf "Sum of invalid IDs %i\n" @@ read_channel ic Day2.sum_of_invalid_ids;
+                        seek_in ic 0;
+                        Printf.printf "Sum of correct invalid IDs %i\n" @@ read_channel ic Day2.sum_of_invalid_ids_2;
                 in
                 Fun.protect ~finally work;
--- day_2/lib/day2.ml
+++ day_2/lib/day2.ml
@@ -19,6 +19,30 @@ module Day2 = struct
                         done;
                         !sum;
                 | None -> 0
+
+        (* Similar to sum_of_invalid_ids but recognizes every repeating pattern *)
+        let sum_of_invalid_ids_2 = function
+                | Some (l, r) ->
+                        let sum = ref 0 in
+                        for i = l to r do
+                                let id = string_of_int i in
+                                let len_sub = String.length id / 2 in
+                                let c_idx = ref 1 in
+                                let quit_loop = ref false in (* TODO: Replace this with recursion *)
+                                        while not !quit_loop && !c_idx <= len_sub do
+                                                let partial = String.make !c_idx '_' in
+                                                let res = Str.global_replace (Str.regexp_string (Str.string_before id !c_idx)) partial id in
+                                                        (* Printf.printf "s/%s/%s/g in %s => %s\n" (Str.string_before id !c_idx) partial id res; *)
+                                                        if res = String.make (String.length id) '_' then
+                                                        begin
+                                                                quit_loop := true;
+                                                                sum := !sum + i
+                                                        end
+                                                        else incr c_idx;
+                                        done
+                        done;
+                        !sum;
+                | None -> 0
 end
 
 let%test _ = Day2.range_from_str "0-1" = Some (0, 1)
@@ -38,3 +62,14 @@ let%test _ = Day2.range_from_str "01" |> Day2.sum_of_invalid_ids = 0
 let%test _ = Day2.range_from_str "0-1" |> Day2.sum_of_invalid_ids = 0
 let%test _ = Day2.range_from_str "10-11" |> Day2.sum_of_invalid_ids = 11
 let%test _ = Day2.range_from_str "10-30" |> Day2.sum_of_invalid_ids = 33
+
+let%test _ = Day2.sum_of_invalid_ids_2 @@ None = 0
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (0, 1) = 0
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (11, 11) = 11
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (111, 111) = 111
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (1111, 1111) = 1111
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (0, 40) = 66
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (121, 121) = 0
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (565656, 565656) = 565656
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (11, 22) = 11 + 22
+let%test _ = Day2.sum_of_invalid_ids_2 @@ Some (95, 115) = 99 + 111
